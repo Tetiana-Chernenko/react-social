7c5a9250cadfb94975202ac228ca5982532c9db8
TC-Dev <teti.cher@gmail.com>
2024-05-30 18:49:39 +0100

    add project react-social to github

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://cdn.dribbble.com/users/5214489/screenshots/20191915/media/fee5c1bccd6150bf85cef3e447e79fd8.jpg?resize=400x300&vertical=center"
+        alt="logo"
+      ></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <img
+        className="background"
+        src="https://img.freepik.com/premium-photo/abstract-floral-watercolor-background-paper_949459-3814.jpg"
+      ></img>
+
+      <div>ava</div>
+
+      <div>My posts</div>
+      <div>Patrion ET 1255</div>
+    </div>
+  );
+};
+
+export default Profile;
